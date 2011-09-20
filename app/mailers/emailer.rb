78c33970f9e5c614a60a32f93792c2c5c8727c3f
Tomasz Skiba <tomasz.r.skiba@accenture.com>
2011-09-20 16:58:08 -0400

    5

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -16,7 +16,7 @@ class Emailer < ActionMailer::Base
   def representative_order_notification(user, order)
     @user = user
     @order = order
-    sendto = "tomasz.r.skiba@accenture.com;tomekskiba@yahoo.com"
+    sendto = "tomasz.r.skiba@accenture.com,tomekskiba@yahoo.com"
     #sendto = sendto + (@user.clinic_email) unless (@user.clinic_email.nil? or @user.clinic_email.blank?)
     #mail(:to => "tomekskiba@yahoo.com",
     mail(#:to => "terry_morabito@baxter.com",
