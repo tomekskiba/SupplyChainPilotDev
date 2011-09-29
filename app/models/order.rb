fe58085a1957c7392261783561c89c75f6563207
Tomasz Skiba <tomasz.r.skiba@accenture.com>
2011-09-29 16:38:24 -0400

    6

--- app/models/order.rb
+++ app/models/order.rb
@@ -282,8 +282,8 @@ class Order < ActiveRecord::Base
     projected_order_quantity = 0 if projected_order_quantity < 0
 
     vals = {
-        "test" => ("on_hand_at_delivery-"on_hand_at_delivery
-    +"</br>usage_per_day-"+usage_per_day
+        "test" => ("on_hand_at_delivery-"+on_hand_at_delivery
+        +"</br>usage_per_day-"+usage_per_day
     +"<br>number_of_days_till_delivery-"+number_of_days_till_delivery
     +"<br>total_days_of_supplies-"+total_days_of_supplies),
         "on_hand_at_delivery" => on_hand_at_delivery,
