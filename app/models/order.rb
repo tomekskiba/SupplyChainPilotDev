50c02368a61637b205992979ab7263e76c469144
Tomasz Skiba <tomasz.r.skiba@accenture.com>
2011-09-29 17:40:02 -0400

    6

--- app/models/order.rb
+++ app/models/order.rb
@@ -268,7 +268,7 @@ class Order < ActiveRecord::Base
     else
       number_of_days_till_delivery = supplies_counted_at_date - delivery_date
     end
-    number_of_days_till_delivery = (44).ceil
+    number_of_days_till_delivery = (how_many_days_till_delivery(number_of_days_till_delivery, number_of_days_in_delivery_cycle)).ceil
     logger.info("solutionsYYYYYYYYYYYYYYYYYYYYY--/"+number_of_days_till_delivery.to_s)
 
     usage_per_week = order_solution_xrefs.find_by_solution_id(solution_id).usage_per_week
@@ -320,6 +320,16 @@ class Order < ActiveRecord::Base
         end
       end
     end
+  end
+
+  def how_many_days_till_delivery(subtract, cycle)
+    subtract2 = subtract
+
+    while subtract2 >= cycle
+      subtract2 -= cycle
+    end
+
+    return subtract2
   end
 
 end
