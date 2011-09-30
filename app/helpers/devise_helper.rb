8fc111588709b1df052f57e9f8ef68214ca24175
Tomasz Skiba <tomasz.r.skiba@accenture.com>
2011-09-30 14:53:25 -0400

    7

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,6 +5,24 @@ module DeviseHelper
   #
   # This method is intended to stay simple and it is unlikely that we are going to change
   # it to add more behavior or options.
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_explanation">
+      <h2>#{sentence}</h2>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
   def devise_error_messages_psw!
     return "" if resource.errors.empty?
 
